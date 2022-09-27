2406fad3cabbccdde8dc461be84cc40d94bc52c0
nn8154 <nn8154@gmail.com>
2022-09-27 22:28:13 +0300

    first commit

--- src/main/java/haagahelia/bookStore/domain/BookRepository.java
+++ src/main/java/haagahelia/bookStore/domain/BookRepository.java
@@ -1,7 +1,11 @@
 package haagahelia.bookStore.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface BookRepository extends CrudRepository <Book, Long> {
+	
+	List<Book> findByAuthor(String author);
 
 }
--- src/main/java/haagahelia/bookStore/web/BookController.java
+++ src/main/java/haagahelia/bookStore/web/BookController.java
@@ -1,11 +1,15 @@
 package haagahelia.bookStore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import haagahelia.bookStore.domain.Book;
 import haagahelia.bookStore.domain.BookRepository;
@@ -13,6 +17,7 @@ import haagahelia.bookStore.domain.CategoryRepository;
 
 @Controller
 public class BookController {
+	
 	@Autowired
 	private BookRepository repository; 
 
@@ -25,6 +30,19 @@ public class BookController {
         model.addAttribute("books", repository.findAll());
         return "booklist";
     }
+    
+	// RESTful service to get all students
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {	
+        return (List<Book>) repository.findAll();
+    }    
+
+	// RESTful service to get student by id
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {	
+    	return repository.findById(bookId);
+    } 
+    
     
     //Add new book
     @RequestMapping(value = "/add")
